8cc7015f09e627df2f394b018c22783457ded2e7
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-11-28 23:31:20 +0530

    added lint staged

--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -1,12 +1,9 @@
 export interface IMainCardProps {}
 
-const MainCard = () => {
-  const a = 0
-  return (
-    <div className='p-4 bg-yellow-700 text-gray-200'>
-      Hello, This is MainCard component!
-    </div>
-  )
-}
+const MainCard = () => (
+  <div className='p-4 text-gray-200 bg-yellow-700'>
+    Hello, This is MainCard component!
+  </div>
+)
 
 export default MainCard
