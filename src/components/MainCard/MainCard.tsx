8b79fd6e5f5bf87de3ddf168f3016b815fde403d
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-11-29 09:02:10 +0530

    feat(package.json): added chromatic

--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -1,7 +1,7 @@
 export interface IMainCardProps {}
 
 const MainCard = () => (
-  <div className='p-4 text-gray-200 bg-yellow-700'>
+  <div className='px-6 py-4 text-gray-200 bg-yellow-700'>
     Hello, This is MainCard component!
   </div>
 )
